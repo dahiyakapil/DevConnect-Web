6c816e51b6a2b436916dba995ee073e1c7987282
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-05-12 15:19:05 +0530

    made the login functionality interactive...
    get the token from the cookies using the withCredentials: true option

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
 
 const Login = () => {
+
+  const [email, setEmail] = useState("elon@gmail.com");
+  const [password, setPassword] = useState("Elon@123");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/auth/login", { email, password }, { withCredentials: true });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='flex justify-center my-10'>
       <div className="card bg-base-300 w-96 shadow-sm">
@@ -8,16 +21,16 @@ const Login = () => {
           <h2 className="card-title flex justify-center">Login</h2>
           <div>
             <fieldset className="fieldset ">
-              <legend className="fieldset-legend">Email</legend>
-              <input type="text" className="input" />
+              <legend className="fieldset-legend">Email </legend>
+              <input type="text" className="input" value={email} onChange={(e) => setEmail(e.target.value)} />
             </fieldset>
             <fieldset className="fieldset my-3 ">
               <legend className="fieldset-legend">Password</legend>
-              <input type="text" className="input" />
+              <input type="text" className="input" value={password} onChange={(e) => setPassword(e.target.value)} />
             </fieldset>
           </div>
           <div className="card-actions justify-center my-2">
-            <button className="btn btn-primary">Login</button>
+            <button onClick={handleLogin} className="btn btn-primary">Login</button>
           </div>
         </div>
       </div>
