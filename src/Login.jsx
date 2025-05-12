8e603473bd307e15e89fcc9ad3a71c9a8ad9d67e
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-05-12 14:28:48 +0530

    Create a structure of login page not interactive..

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      Login Page
+    <div className='flex justify-center my-10'>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset ">
+              <legend className="fieldset-legend">Email</legend>
+              <input type="text" className="input" />
+            </fieldset>
+            <fieldset className="fieldset my-3 ">
+              <legend className="fieldset-legend">Password</legend>
+              <input type="text" className="input" />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center my-2">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
